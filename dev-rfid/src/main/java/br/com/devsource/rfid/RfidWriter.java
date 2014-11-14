d44bc15c3195768bc3fdc40f6a6480e418fab8a3
Guilherme Pacheco <guilherfp@gmail.com>
2014-11-14 18:10:58 -0200

    Importação inicial

---
+++ dev-rfid/src/main/java/br/com/devsource/rfid/RfidWriter.java
@@ -0,0 +1,5 @@
+package br.com.devsource.rfid;
+
+public interface RfidWriter {
+
+}
---
+++ dev-rfid/src/main/java/br/com/devsource/rfid/tag/Tag.java
@@ -0,0 +1,20 @@
+package br.com.devsource.rfid.tag;
+
+import org.apache.commons.lang3.Validate;
+
+public class Tag {
+
+  private String epc;
+
+  Tag() {}
+
+  public Tag(String epc) {
+    Validate.notBlank(epc);
+    this.epc = epc;
+  }
+
+  public String getEpc() {
+    return epc;
+  }
+
+}
