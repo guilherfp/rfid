207b6a41ad09e132860598836634c8a7d65faef9
Guilherme Pacheco <guilherfp@gmail.com>
2014-11-18 09:20:21 -0200

    Signed-off-by: Guilherme Pacheco <guilherfp@gmail.com>

--- dev-rfid-epc/src/main/java/br/com/devsource/rfid/epc/EpcDecoder.java
+++ dev-rfid-epc/src/main/java/br/com/devsource/rfid/epc/EpcDecoder.java
@@ -1,9 +1,7 @@
 package br.com.devsource.rfid.epc;
 
-import br.com.devsource.rfid.tag.Tag;
-
 public interface EpcDecoder {
 
-  String decodeCodeBar(Tag tag);
+  String toCodeBar(String epc);
 
 }
--- dev-rfid/src/main/java/br/com/devsource/rfid/tag/Tag.java
+++ dev-rfid/src/main/java/br/com/devsource/rfid/tag/Tag.java
@@ -10,11 +10,33 @@ public final class Tag {
 
   public Tag(String epc) {
     Validate.notBlank(epc);
-    this.epc = epc;
+    this.epc = epc.toUpperCase();
   }
 
   public String getEpc() {
     return epc;
+  }
+
+  @Override
+  public int hashCode() {
+    return epc.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if ((obj == null) || (obj.getClass() != Tag.class)) {
+      return false;
+    }
+    Tag other = (Tag) obj;
+    return epc.equals(other.epc);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Tag = epc: %s", epc);
   }
 
 }
