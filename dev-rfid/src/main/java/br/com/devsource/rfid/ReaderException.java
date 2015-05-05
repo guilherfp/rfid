36015be9ee0bf304c2f62630df1d143213a56d41
Guilherme Freitas Pacheco <guilherfp@gmail.com>
2015-05-05 00:35:49 -0300

    Signed-off-by: Guilherme Freitas Pacheco <guilherfp@gmail.com>

---
+++ dev-rfid/src/main/java/br/com/devsource/rfid/ReaderException.java
@@ -0,0 +1,22 @@
+package br.com.devsource.rfid;
+
+public class ReaderException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public ReaderException() {
+    super();
+  }
+
+  public ReaderException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ReaderException(String message) {
+    super(message);
+  }
+
+  public ReaderException(Throwable cause) {
+    super(cause);
+  }
+
+}
