27b0ba2c63f5882873fb3f6019616ead99d8ada4
Guilherme Freitas Pacheco <guilherfp@gmail.com>
2015-07-16 00:26:03 -0300

    Signed-off-by: Guilherme Freitas Pacheco <guilherfp@gmail.com>

--- dev-rfid/src/main/java/br/com/devsource/rfid/bri/BRIReader.java
+++ dev-rfid/src/main/java/br/com/devsource/rfid/bri/BRIReader.java
@@ -1,8 +1,5 @@
 package br.com.devsource.rfid.bri;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import br.com.devsource.rfid.AbstractRfid;
 import br.com.devsource.rfid.ReaderConfig;
 import br.com.devsource.rfid.RfidDevice;
@@ -12,10 +9,6 @@ import br.com.devsource.rfid.RfidModule;
  * @author Guilherme Pacheco
  */
 public class BRIReader extends AbstractRfid implements RfidModule {
-
-  private static final String CMD_OK = "OK>";
-  private String schema = "ANT,EPCID,STR(3:0,32)";
-  private static final Logger LOGGER = LoggerFactory.getLogger(BRIReader.class);
 
   public BRIReader(ReaderConfig config) {
     super(config);
