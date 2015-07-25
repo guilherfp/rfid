575710fcae858e887dd6bf55d9c814c66740bd27
Guilherme Freitas Pacheco <guilherfp@gmail.com>
2015-07-25 01:13:43 -0300

    Signed-off-by: Guilherme Freitas Pacheco <guilherfp@gmail.com>

--- dev-rfid-parent/dev-rfid/src/test/java/br/com/devsource/rfid/ReaderUtilsTest.java
+++ dev-rfid-parent/dev-rfid/src/test/java/br/com/devsource/rfid/ReaderUtilsTest.java
@@ -1,8 +1,32 @@
 package br.com.devsource.rfid;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * @author Guilherme Pacheco
  */
 public class ReaderUtilsTest {
+
+  @Test
+  public void testScale() throws Exception {
+    assertEquals(15, ReaderUtils.scale(50, 30));
+  }
+
+  @Test
+  public void testScale_ValorNegativo() throws Exception {
+    assertEquals(0, ReaderUtils.scale(-1, 30));
+  }
+
+  @Test
+  public void testScale_ValorMaximo() throws Exception {
+    assertEquals(30, ReaderUtils.scale(100, 30));
+  }
+
+  @Test
+  public void testScale_AcimaDoMaximo() throws Exception {
+    assertEquals(30, ReaderUtils.scale(150, 30));
+  }
 
 }
