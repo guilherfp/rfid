5799a142df1d8ac5e6773327a3c83bb17395f70d
Guilherme Pacheco <guilherfp@gmail.com>
2016-02-23 21:04:25 -0300

    Signed-off-by: Guilherme Pacheco <guilherfp@gmail.com>

---
+++ rfid-api/src/main/java/br/com/devsource/rfid/api/module/ReadMode.java
@@ -0,0 +1,25 @@
+package br.com.devsource.rfid.api.module;
+
+/**
+ * @author Guilherme Pacheco
+ */
+public enum ReadMode {
+
+  UNIQUE("Event read unique tag"),
+  ALL("Event read all tag");
+
+  private String description;
+
+  private ReadMode(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public String toString() {
+    return description;
+  }
+}
---
+++ rfid-api/src/main/java/br/com/devsource/rfid/api/tag/TagField.java
@@ -0,0 +1,9 @@
+package br.com.devsource.rfid.api.tag;
+
+public enum TagField {
+
+  EPCID,
+  TAGID,
+  ANTENNA;
+
+}
